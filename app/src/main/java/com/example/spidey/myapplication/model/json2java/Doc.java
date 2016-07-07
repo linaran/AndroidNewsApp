cc567d2619000d412901c14db7d246bd6206d0ae
Deni <deni.munjas@gmail.com>
2016-07-07 12:31:35 +0200

    Remove several fields from Doc class (mainly null, empty arrays, metadata). Remove class Person since it is no longer being used anywhere.

--- app/src/main/java/com/example/spidey/myapplication/model/json2java/Doc.java
+++ app/src/main/java/com/example/spidey/myapplication/model/json2java/Doc.java
@@ -19,18 +19,6 @@ public final class Doc {
     @Expose
     private String leadParagraph;
 
-    @SerializedName("abstract")
-    @Expose
-    private Object _abstract;
-
-    @SerializedName("print_page")
-    @Expose
-    private Object printPage;
-
-    @SerializedName("blog")
-    @Expose
-    private List<Object> blog = new ArrayList<>();
-
     @SerializedName("source")
     @Expose
     private String source;
@@ -43,45 +31,9 @@ public final class Doc {
     @Expose
     private Headline headline;
 
-    @SerializedName("keywords")
-    @Expose
-    private List<Object> keywords = new ArrayList<>();
-
-    @SerializedName("pub_date")
-    @Expose
-    private String pubDate;
-
-    @SerializedName("document_type")
-    @Expose
-    private String documentType;
-
-    @SerializedName("news_desk")
-    @Expose
-    private String newsDesk;
-
-    @SerializedName("section_name")
-    @Expose
-    private String sectionName;
-
-    @SerializedName("subsection_name")
-    @Expose
-    private Object subsectionName;
-
-    @SerializedName("type_of_material")
-    @Expose
-    private String typeOfMaterial;
-
     @SerializedName("_id")
     @Expose
     private String id;
-
-    @SerializedName("word_count")
-    @Expose
-    private String wordCount;
-
-    @SerializedName("slideshow_credits")
-    @Expose
-    private Object slideshowCredits;
 
     public String getWebUrl() {
         return webUrl;
@@ -105,31 +57,6 @@ public final class Doc {
 
     public void setLeadParagraph(String leadParagraph) {
         this.leadParagraph = leadParagraph;
-    }
-
-    public Object getAbstract() {
-        return _abstract;
-    }
-
-    public void setAbstract(Object _abstract) {
-        this._abstract = _abstract;
-    }
-
-    public Object getPrintPage() {
-        return printPage;
-    }
-
-    public void setPrintPage(Object printPage) {
-        this.printPage = printPage;
-    }
-
-
-    public List<Object> getBlog() {
-        return blog;
-    }
-
-    public void setBlog(List<Object> blog) {
-        this.blog = blog;
     }
 
     public String getSource() {
@@ -156,84 +83,11 @@ public final class Doc {
         this.headline = headline;
     }
 
-    public List<Object> getKeywords() {
-        return keywords;
-    }
-
-    public void setKeywords(List<Object> keywords) {
-        this.keywords = keywords;
-    }
-
-
-    public String getPubDate() {
-        return pubDate;
-    }
-
-    public void setPubDate(String pubDate) {
-        this.pubDate = pubDate;
-    }
-
-    public String getDocumentType() {
-        return documentType;
-    }
-
-    public void setDocumentType(String documentType) {
-        this.documentType = documentType;
-    }
-
-    public String getNewsDesk() {
-        return newsDesk;
-    }
-
-    public void setNewsDesk(String newsDesk) {
-        this.newsDesk = newsDesk;
-    }
-
-    public String getSectionName() {
-        return sectionName;
-    }
-
-    public void setSectionName(String sectionName) {
-        this.sectionName = sectionName;
-    }
-
-    public Object getSubsectionName() {
-        return subsectionName;
-    }
-
-    public void setSubsectionName(Object subsectionName) {
-        this.subsectionName = subsectionName;
-    }
-
-    public String getTypeOfMaterial() {
-        return typeOfMaterial;
-    }
-
-    public void setTypeOfMaterial(String typeOfMaterial) {
-        this.typeOfMaterial = typeOfMaterial;
-    }
-
     public String getId() {
         return id;
     }
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getWordCount() {
-        return wordCount;
-    }
-
-    public void setWordCount(String wordCount) {
-        this.wordCount = wordCount;
-    }
-
-    public Object getSlideshowCredits() {
-        return slideshowCredits;
-    }
-
-    public void setSlideshowCredits(Object slideshowCredits) {
-        this.slideshowCredits = slideshowCredits;
     }
 }
